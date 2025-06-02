8a89cf0cfef9e3f8bff136277c1f72b6a0fc4109
Varsha-9014 <jagadalavarsha@gmail.com>
2025-06-02 10:08:53 +0530

    Add files via upload

---
+++ RainUmbrella.java
@@ -0,0 +1,22 @@
+package xyz;
+
+import java.util.Scanner;
+
+public class RainUmbrella {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the age: ");
+        int age = sc.nextInt(); 
+
+        if (age >18) {
+            System.out.println("Eligible for vote");
+        } else {
+            System.out.println("Not eligible");
+        }
+
+        sc.close(); 
+    }
+
+}
