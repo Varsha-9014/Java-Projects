ce8628796875118949db9c4da1ae8d1a1565e4d6
Varsha-9014 <jagadalavarsha@gmail.com>
2025-06-03 15:02:12 +0530

    Add files via upload

---
+++ FavoriteCandies.java
@@ -0,0 +1,15 @@
+package xyz;
+
+import java.util.LinkedHashSet;
+
+public class FavoriteCandies {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> candies=new LinkedHashSet<String>();
+		candies.add("chocolate");
+		candies.add("watermelon");
+		candies.add("mango");
+		System.out.println(candies);
+	}
+
+}
