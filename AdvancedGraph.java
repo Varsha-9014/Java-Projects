dc0c29054e060503ad5cd54f622a942cd75959f1
Varsha-9014 <jagadalavarsha@gmail.com>
2025-06-05 15:53:38 +0530

    Add files via upload

---
+++ AdvancedGraph.java
@@ -0,0 +1,39 @@
+package xyz;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdvancedGraph {
+	static class graph{
+		private Map<Integer,List<Integer>> adjlist=new HashMap<Integer,List<Integer>>();
+		void addEdge(int u,int v) {
+			adjlist.computeIfAbsent(u, k->new ArrayList<>()).add(v);
+			adjlist.computeIfAbsent(v, k->new ArrayList<>()).add(u);
+			
+		}
+		void printGraph(){
+			for(int node:adjlist.keySet()) {
+				System.out.println("Node"+node+"is connected to:");
+			for(int neighbor:adjlist.get(node)) {
+				System.out.println(neighbor+" ");
+			}
+				
+			}
+			
+		}
+		
+	}
+	public static void main(String[] args) {
+			graph g1=new graph();
+			g1.addEdge(2, 2);
+			g1.addEdge(1, 3);
+			g1.addEdge(2, 4);
+			g1.addEdge(1, 4);
+			g1.printGraph();
+
+		
+
+	}
+
+}
