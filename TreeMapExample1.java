9b9f79367e0f7f4dd9a2ac72cfb9e2abbd33bcb3
Varsha-9014 <jagadalavarsha@gmail.com>
2025-06-05 13:54:28 +0530

    Add files via upload

---
+++ TreeMapExample1.java
@@ -0,0 +1,47 @@
+package xyz;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class TreeMapExample1 {
+    public static void main(String[] args) {
+        TreeMap<Integer, Integer> studentMarks = new TreeMap<>();
+        studentMarks.put(101, 85);
+        studentMarks.put(102, 90);
+        studentMarks.put(103, 78);
+
+        System.out.println("Student Marks:");
+        for (Integer roll : studentMarks.keySet()) {
+            System.out.println("Roll No: " + roll + ", Marks: " + studentMarks.get(roll));
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter roll number to search: ");
+        int searchRoll = sc.nextInt();
+
+        if (studentMarks.containsKey(searchRoll)) {
+            System.out.println("Marks for Roll No " + searchRoll + ": " + studentMarks.get(searchRoll));
+        } else {
+            System.out.println("Roll No " + searchRoll + " not found.");
+        }
+        TreeMap<String, String> phoneBook = new TreeMap<>();
+        phoneBook.put("Varsha", "9876543210");
+        phoneBook.put("Honey", "9123456780");
+        phoneBook.put("Lucky", "9000011111");
+
+        System.out.println("\nPhone Book:");
+        for (String name : phoneBook.keySet()) {
+            System.out.println("Name: " + name + ", Phone: " + phoneBook.get(name));
+        }
+
+        System.out.print("\nEnter name to search in phone book: ");
+        sc.nextLine(); 
+        String searchName = sc.nextLine();
+
+        if (phoneBook.containsKey(searchName)) {
+            System.out.println("Phone number of " + searchName + ": " + phoneBook.get(searchName));
+        } else {
+            System.out.println(searchName + " not found in phone book.");
+        }
+
+        sc.close();
+    }
+}
