4f8e06df83a6362e424c392fe987be2185d15f5c
Varsha-9014 <jagadalavarsha@gmail.com>
2025-06-05 13:52:13 +0530

    Add files via upload

---
+++ FriendsGraph.java
@@ -0,0 +1,22 @@
+package xyz;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FriendsGraph {
+
+	public static void main(String[] args) {
+		Map<String, List<String>>graph =new HashMap<>();
+		graph.put("Alice",Arrays.asList("Bob"));
+		graph.put("Bob",Arrays.asList("Alice","Varsha"));
+		graph.put("Varsha",Arrays.asList("Bob"));
+		
+		for(String person:graph.keySet()) {
+			System.out.println(person+"is friends with"+graph.get(person));
+		}
+		
+
+	}
+
+}
